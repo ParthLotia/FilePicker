cfdd87fd8ffcf71d6d1e85ca4e1d9043393ea907
parth <lotiaparth@gmail.com>
2024-03-23 23:09:09 +0530

    Gradle and changes.

--- README.md
+++ README.md
@@ -11,14 +11,11 @@ I have tested with multiple scenarios , here is the scenario list
 3. Single Photo Selection Working on All Android Versions
 4. Multiple Photo Selection will work from Android 11 to Android 13.
 5. Multiple Photo Selection below Android 11 we have to use clipdata the old way.
-6. Single Selection of PDf and Document as well. You can preview of PDF you have selected.
-7. Single Selection of Music and Video File as well.
-7. For all above points you don't require a single permission.
 
 <!--![LocationPermission](art/ss_location1.png)-->
 
-<!--<p align="center">
+<p align="center">
   <img src="art/ss_photopicker1.png" width="350">
   <img src="art/ss_photopicker2.png" width="350">
   <img src="art/ss_photopicker3.png" width="350">
-</p>-->
+</p>
--- app/src/main/java/com/android/parth/NewPhotoPicker.kt
+++ app/src/main/java/com/android/parth/NewPhotoPicker.kt
@@ -1,82 +1,122 @@
 package com.android.parth
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import androidx.lifecycle.lifecycleScope
+import com.plandroid.photopicker.FileUriUtils
+import com.plandroid.photopicker.FileUtil
+import com.plandroid.photopicker.TakePictureWithUriReturnContract
 import java.io.File
+
 
 class NewPhotoPicker : AppCompatActivity() {
 
     lateinit var img_pick: ImageView
     lateinit var btn_upload_pick: Button
+    // lateinit var btn_upload_camera: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_photo_picker)
 
         img_pick = findViewById(R.id.img_pick)
+
         btn_upload_pick = findViewById(R.id.btn_upload_pick)
 
-
         btn_upload_pick.setOnClickListener {
-
             val mimeType = "*/*"
-
             /*Single Document Picker*/
             // Image , Video , PDF , DOC , DOCX
             pickMedia.launch(
                 arrayOf(mimeType)
             )
-
-            /* Multiple Document Picker*/
-            //pickMultipleDocument.launch(arrayOf(mimeType))
-
-            /*Multiple Image And Video*/
-            //pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
-
-            /*Multiple Image*/
-            //pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-
-            /*Multiple Video*/
-            //pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly))
-
-
         }
-
-
     }
 
-    private val pickMedia = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
-        if (uri != null) {
-            Log.e("PhotoPicker", "Selected URI: $uri")
+    // For Camera uncomment below code
+    /*btn_upload_camera = findViewById(R.id.btn_upload_camera)*/
+    /*btn_upload_camera.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            } else {
+                takePicture()
+            }
+        }*/
+    /* private fun takePicture() {
+         lifecycleScope.launchWhenStarted {
+             getTmpFileUri().let { uri ->
+                 pickCamera.launch(uri)
+             }
+         }
+     }*/
 
-            val file = FileUriUtils.getRealPath(this, uri)?.let { File(it) }
-            Log.e("file", "" + file?.exists())
-            Log.e("fileLength", "" + file?.length())
-            Log.e("fileName", "" + file?.name)
-            Log.e("filePath", "" + file?.path)
-            Log.e("file.extension", "" + file?.extension)
+    /*private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            takePicture()
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.CAMERA
+                )
+            ) {
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+            } else {
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                val uri = Uri.fromParts("package", this.packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+        }
+    }*/
 
-            if (file != null) {
+    /*  private val pickCamera = registerForActivityResult(TakePictureWithUriReturnContract()) { (isSuccess, imageUri) ->
+              if (isSuccess) {
+                  img_pick.setImageURI(imageUri)
+                  val file = FileUriUtils.getRealPath(this, imageUri)?.let { File(it) }
+                  Log.e("file", "" + file?.exists())
+                  Log.e("fileLength", "" + file?.length())
+                  Log.e("fileName", "" + file?.name)
+                  Log.e("filePath", "" + file?.path)
+                  Log.e("file.extension", "" + file?.extension)
+              }
+          }*/
 
-                if ((file.extension.equals("pdf", true)) ||
-                    (file.extension.equals("doc", true)) ||
-                    (file.extension.equals("docx", true)) ||
-                    (file.extension.equals("mp4", true)) ||
-                    (file.extension.equals("mp3", true)) ||
-                    (file.extension.equals("eac3", true)) ||
-                    (file.extension.equals("wav", true)) ||
-                    (file.extension.equals("mov", true)) ||
-                    (file.extension.equals("avi", true)) ||
-                    (file.extension.equals("mkv", true)) ||
-                    (file.extension.equals("webm", true))
-                ) {
+     /*private fun getTmpFileUri(): Uri {
+         val tmpFile = File.createTempFile("tmp_image_file", ".png", cacheDir).apply {
+             createNewFile()
+             deleteOnExit()
+         }
 
+         return FileProvider.getUriForFile(
+             applicationContext,
+             "${applicationContext.packageName}.fileProvider",
+             tmpFile
+         )
+     }*/
+
+
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+            if (uri != null) {
+                val file = FileUriUtils.getRealPath(this, uri)?.let { File(it) }
+                if (FileUriUtils.checkExtensionFile(file)) {
+                    img_pick.setImageURI(uri)
+                } else {
                     img_pick.setImageBitmap(
                         FileUtil.getThumbnail(
                             file,
@@ -84,58 +124,9 @@ class NewPhotoPicker : AppCompatActivity() {
                             context = applicationContext
                         )
                     )
-
-                } else if ((file.extension.equals("jpg", true)) ||
-                    (file.extension.equals("jpeg", true)) ||
-                    (file.extension.equals("png", true))
-                ) {
-
-                    img_pick.setImageURI(uri)
-
-                }
-            } else {
-                img_pick.setImageResource(R.drawable.img_not_supported)
-                Toast.makeText(this, "This file format is not supported", Toast.LENGTH_SHORT).show()
-            }
-
-
-        } else {
-            Log.d("PhotoPicker", "No media selected")
-        }
-    }
-
-
-    // This Multiple Selection working from Android 11 to 13
-    // Below Android 11 we need to use old way clip data to achieve it
-
-    val pickMultipleMedia =
-        registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
-            if (uris.isNotEmpty()) {
-                if (uris.size > 5) {
-                    Toast.makeText(this, "Please Select Item Upto 5", Toast.LENGTH_SHORT).show()
-                } else {
-                    Log.d("PhotoPicker", "Number of items selected: ${uris.size}")
-                    Log.d("PhotoPicker", "Number of items selected: ${uris}")
                 }
             } else {
                 Log.d("PhotoPicker", "No media selected")
             }
-        }
-
-    private val pickMultipleDocument =
-        registerForActivityResult(ActivityResultContracts.OpenMultipleDocuments()) {uris ->
-            if (uris.isNotEmpty()){
-
-                for (i in uris.indices){
-                    val file = FileUriUtils.getRealPath(this, uris[i])?.let { File(it) }
-                    Log.e("file", "" + file?.exists())
-                    Log.e("fileLength", "" + file?.length())
-                    Log.e("fileName", "" + file?.name)
-                    Log.e("filePath", "" + file?.path)
-                    Log.e("file.extension", "" + file?.extension)
-                }
-
-            }
-
         }
 }
